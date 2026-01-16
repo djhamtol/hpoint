b781dfd572d54a36587ba6c7b9d6eb979fb1e37a
gamza <rachu970605@gmail.com>
2026-01-16 19:51:57 +0900

    헤더 완성

--- js/stack.js
+++ js/stack.js
@@ -25,8 +25,8 @@ $(function () {
 
     });
 
-    // mbGnb depth2 열기
-    $('#mbMenuArea #mbGnb').on('click', 'li.depth1', function(){
+    // mbGnb 하위 메뉴 열기
+    $('#mbGnb').on('click', 'li.depth1', function(){
 
         $(this).addClass('active').siblings().removeClass('active');
 
@@ -35,5 +35,41 @@ $(function () {
         $(this).siblings().find('ul.depth2').hide();
 
     });
-    
+
+    // pcGnb 하위 메뉴 열기
+    $('#pcGnb').on('mouseenter', function(){
+
+        $(this).addClass('active')
+        .find('.depth2').stop().slideDown(200);
+
+        $('.pcGnbBg').stop().animate({ height: 500 }, 200);
+
+        $('.headerDim').stop().fadeIn(200);
+
+    });
+
+    // pcGnb 하위 메뉴 닫기
+    $('header').on('mouseleave', function(){
+
+        $(this).removeClass('active')
+        .find('.depth2').stop().slideUp(200);
+
+        $('.pcGnbBg').stop().animate({ height: 0 }, 200);
+
+        $('.headerDim').stop().fadeOut(200);
+        
+    });
+
+    // pcGnb 메뉴&하위 메뉴 클릭시 스타일링
+    $('#pcGnb > ul > li .depth1 , #pcGnb .depth2 li a').on('click', function () {
+
+        $('#pcGnb > ul > li .depth1').removeClass('active');
+
+        $(this)
+        .closest('#pcGnb > ul > li')
+        .children('.depth1')
+        .addClass('active');
+
+    });
+
 });
